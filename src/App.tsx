034bcdba8e620d05e714cce233e446a55200a069
Akhmad Setiawan <13521164@mahasiswa.itb.ac.id>
2025-02-18 12:25:40 +0700

    feat: router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import './App.css'
+
+import {
+  Routes,
+  Route
+} from "react-router-dom"
+
+// ** Client Pages
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reloadsd.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home /> } />
+      {/* <Route path="/not-found" element={<Layout> <NotFound /> </Layout>} />
+      <Route path="/login" element={<Layout> <Login /> </Layout>} />
+      <Route path="/register" element={<Layout> <Register /> </Layout>} /> */}
+
+      {/* Catch-all route for not-found */}
+      {/* <Route path="*" element={<NotFoundPageHandler />} /> */}
+    </Routes>
   );
 }
 
